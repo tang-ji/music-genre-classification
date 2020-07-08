6fa59423d9dab50e527dc794492228c8ed473407
tangji08 <ji.tang@polytechnique.edu>
2020-07-08 22:25:58 +0200

    v1.0

---
+++ DataLoader.py
@@ -0,0 +1,96 @@
+import pandas as pd
+import numpy as np
+import pickle
+import os
+
+from librosa.core import load
+from librosa.feature import melspectrogram
+from librosa import power_to_db
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.utils import shuffle
+
+RAW_DATAPATH="genres_raw"
+class Label():
+    def __init__(self, genres_list):
+        self.le         = LabelEncoder()
+        integer_encoded = self.le.fit_transform(genres_list)
+        self.one        = OneHotEncoder()
+        self.one.fit(integer_encoded.reshape(len(integer_encoded), 1))
+        
+    def transform(self, Y):
+        Y = self.one.transform(np.expand_dims(self.le.transform(Y), axis=1))
+        return Y
+
+class Data():
+    def __init__(self, genres):
+        self.raw_data = None
+        self.genres_list = genres
+        self.train_set  = None
+        self.test_set   = None
+        self.label = Label(self.genres_list)
+        
+
+    def load_data(self, datapath):
+        self.datapath = datapath
+        records = list()
+        for i, genre in enumerate(self.genres_list):
+            GENREPATH = self.datapath + genre + "/"
+            for j, track in enumerate(os.listdir(GENREPATH)):
+                TRACKPATH   = GENREPATH + track
+                print("%d.%s\t\t%s (%d)" % (i + 1, genre, TRACKPATH, j + 1))
+                y, sr       = load(TRACKPATH, mono=True)
+                S           = melspectrogram(y, sr).T
+                S           = S[:-1 * (S.shape[0] % 128)]
+                num_chunk   = S.shape[0] / 128
+                data_chunks = np.split(S, num_chunk)
+                data_chunks = [(data, genre) for data in data_chunks]
+                records.append(data_chunks)
+
+        records = [data for record in records for data in record]
+        self.raw_data = pd.DataFrame.from_records(records, columns=['spectrogram', 'genre'])
+      
+    def build_dataset(self):
+        df = self.raw_data.copy()
+        df = shuffle(df)
+
+        train_records, test_records = list(), list()
+        for i, genre in enumerate(self.genres_list):
+            genre_df    = df[df['genre'] == genre]
+            train_records.append(genre_df.iloc[:900].values)
+            test_records.append(genre_df.iloc[900:].values)
+
+        train_records   = shuffle([record for genre_records in train_records    for record in genre_records])
+        test_records    = shuffle([record for genre_records in test_records     for record in genre_records])
+
+        self.train_set  = pd.DataFrame.from_records(train_records,  columns=['spectrogram', 'genre'])
+        self.test_set   = pd.DataFrame.from_records(test_records,   columns=['spectrogram', 'genre'])
+        
+    def get_train_set(self):
+        x_train = np.stack(self.train_set['spectrogram'].values)
+        x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], x_train.shape[2]))
+        y_train = np.stack(self.train_set['genre'].values)
+        y_train = self.label.transform(y_train)
+        print("x_train shape: ", x_train.shape)
+        print("y_train shape: ", y_train.shape)
+        return x_train, y_train
+
+    def get_test_set(self):
+        x_test  = np.stack(self.test_set['spectrogram'].values)
+        x_test  = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], x_test.shape[2]))
+        y_test  = np.stack(self.test_set['genre'].values)
+        y_test  = self.label.transform(y_test)
+        print("x_test shape : ", x_test.shape)
+        print("y_test shape : ", y_test.shape)
+        return x_test, y_test
+
+    def save(self):
+        with open(RAW_DATAPATH, 'wb') as outfile:
+            pickle.dump(self.raw_data, outfile, pickle.HIGHEST_PROTOCOL)
+        print('-> Data() object is saved.\n')
+        return
+
+    def load(self):
+        with open(RAW_DATAPATH, 'rb') as infile:
+            self.raw_data   = pickle.load(infile)
+        print("-> Data() object is loaded.")
+        return
--- model.py
+++ model.py
@@ -3,8 +3,10 @@ from keras.models import Sequential, Model
 from keras.layers.normalization import BatchNormalization
 from keras.layers import *
 
+
 def conv(layer, filters, kernel_size, pool_size):
-    x = Conv2D(filters, kernel_size=kernel_size, activation="relu")(layer)
+    x = Conv2D(filters, kernel_size=kernel_size)(layer)
+    x = keras.layers.LeakyReLU(alpha=0.1)(x)
     x = BatchNormalization()(x)
     x = MaxPooling2D(pool_size=pool_size)(x)
     return x
@@ -15,28 +17,42 @@ def createModelCNN():
 
     conv1 = conv(input_reshape, 64, (3,3), (2,2))
     conv2 = conv(conv1, 128, (3,3), (2,2))
-    conv3 = conv(conv2, 256, (3,3), (4,4))
+    conv3 = conv(conv2, 256, (5,5), (2,2))
     conv4 = conv(conv3, 512, (3,3), (4,4))
 
     flatten = Flatten()(conv4)
     dense1 = Dropout(0.5)(Dense(1024)(flatten))
+    dense1 = keras.layers.LeakyReLU(alpha=0.1)(dense1)
     dense2 = Dropout(0.5)(Dense(256)(dense1))
+    dense2 = keras.layers.LeakyReLU(alpha=0.1)(dense2)
 
     output = Dense(10, activation="softmax")(dense2)
 
     return Model(input, output)
 
-class ModelCNN:
-    def __init__():
+
+class ModelCNN():
+    def __init__(self):
         self.model = None
         
-    def build():
+    def build(self):
         self.model = createModelCNN()
         self.model.compile(loss='categorical_crossentropy', optimizer='adam')
         
-    def save(path='model/'):
+    def predict_music(self, music_path):
+        y, sr       = load(music_path, mono=True)
+        S           = melspectrogram(y, sr).T
+        S           = S[:-1 * (S.shape[0] % 128)]
+        num_chunk   = S.shape[0] / 128
+        data_chunks = np.split(S, num_chunk)
+        x_pred = np.stack(data_chunks)
+        y_pred = np.mean(self.model.predict(x_pred), axis=0)
+        for i in range(len(genres_list)):
+            print("{:10s} {:6.2f}%".format(genres_list[i], y_pred[i]*100))
+        
+    def save(self, path='model/'):
         self.model.save(path + 'CNN_model')
              
-    def load(path='model/'):
+    def load(self, path='model/'):
         self.model = keras.models.load_model(path + 'CNN_model')
 
